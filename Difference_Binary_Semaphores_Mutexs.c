2c91ca84df63afedf801560f9b4fea6a3f6633ad
MathDeyvi <deyvisson.moreno@ime.eb.br>
2025-01-14 11:01:53 -0300

    17° Aula Diferença entre Semáforos e Mutex

---
+++ Difference_Binary_Semaphores_Mutexs.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <semaphore.h>
+
+/*
+
+Característica | Semáforo Binário | Mutex
+
+Propriedade	| Qualquer thread pode liberar | Apenas a thread que bloqueou pode liberar
+
+Finalidade | Usado para sincronização ou controle de acesso | Usado principalmente para proteger recursos compartilhados
+
+Peso | Leve, mas menos focado em exclusividade | Projetado para exclusão mútua rigorosa.
+
+Sinalização | Pode ser usado para sinalizar entre threads. | Não é usado para sinalização, apenas para acesso exclusivo.
+
+*/
+
+
+
+
+int main(){
+	
+	return 0;
+}
